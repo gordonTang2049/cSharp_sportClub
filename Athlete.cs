277ef969e3b9d67f7859bf6914495db520b6f1f6
root <root@DESKTOP-LM556MP.localdomain>
2022-10-31 21:40:17 +1100

    update

---
+++ Athlete.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QutChampionship_n11267283
+{
+    internal class Athlete
+    {
+        public static char[] _eventCodes = { 'T', 'B', 'S', 'R', 'G' };
+        public static string[] _eventDescription = { "Tennis", "Badminton", "Swimming", "Running", "Gymnastic" };
+        private string name;
+        private char eventCode;
+        private string eventDescription;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public char EventCode
+        {
+            get { return eventCode; }
+        }
+
+        public string EventDescription
+        {
+            get { return eventDescription; }
+        }
+
+
+
+        public Athlete(string name, char eventCode)
+        {
+            this.name = name;
+
+            if (eventCode == _eventCodes[0])
+            {
+                this.eventCode = _eventCodes[0];
+                this.eventDescription = _eventDescription[0];
+
+            }
+            else if (eventCode == _eventCodes[1])
+            {
+
+                this.eventCode = _eventCodes[1];
+                this.eventDescription = _eventDescription[1];
+
+            }
+            else if (eventCode == _eventCodes[2])
+            {
+
+                this.eventCode = _eventCodes[2];
+                this.eventDescription = _eventDescription[2];
+
+            }
+            else if (eventCode == _eventCodes[3])
+            {
+
+                this.eventCode = _eventCodes[3];
+                this.eventDescription = _eventDescription[3];
+
+            }
+            else if (eventCode == _eventCodes[4])
+            {
+
+                this.eventCode = _eventCodes[4];
+                this.eventDescription = _eventDescription[4];
+
+            }
+            else
+            {
+                this.eventCode = 'I';
+                this.eventDescription = "Invalid";
+            }
+
+        }
+
+        public void Display()
+        {
+
+            Console.WriteLine("Those are the event codes");
+            for (int i = 0; i < _eventCodes.Length; i++)
+            {
+                Console.Write($"{_eventCodes[i].ToString()} ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < _eventDescription.Length; i++)
+            {
+                Console.Write($"{_eventDescription[i].ToString()} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Please enter one of the Corresponding event code");
+        }
+
+        public override string ToString()
+        {
+            return (Name + " has type event code **" + EventCode + "** which event is **" + EventDescription + "** \n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QutChampionship_n11267283
+{
+    internal class Program
+    {
+        static void validateNumOfParticipants(out int numOfParticipants)
+        {
+            int.TryParse(Console.ReadLine(), out numOfParticipants);
+
+            if (numOfParticipants < 0 | numOfParticipants > 30)
+            {
+
+                Console.WriteLine("The value you are enter is invalid, please enter number between 0 and 30");
+                numOfParticipants = -1;
+            }
+
+        }
+
+        static void revDisplay(int numOfParticipants)
+        {
+
+            Console.WriteLine(numOfParticipants * 20);
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int numOfParticipants = -1;
+            char eventCodeInput;
+            Athlete display = new Athlete("", 'i');
+
+
+            Console.WriteLine("please enter Paricipants number between 0 and 30");
+
+            while (numOfParticipants == -1)
+            {
+                validateNumOfParticipants(out numOfParticipants);
+            }
+
+            revDisplay(numOfParticipants);
+
+            Athlete[] athletes = new Athlete[numOfParticipants];
+
+            Console.WriteLine("==============================");
+            Console.WriteLine("Requirement d. ");
+            Console.WriteLine("==============================");
+            for (int i = 0; i < numOfParticipants; i++)
+            {
+
+                Console.WriteLine("Please enter your name");
+                string name = Console.ReadLine();
+                display.Display();
+                Char.TryParse(Console.ReadLine(), out eventCodeInput);
+
+                athletes[i] = new Athlete(name, eventCodeInput);
+
+                Console.WriteLine(athletes[i].ToString());
+
+            }
+            Console.WriteLine("==============================");
+            Console.WriteLine("Requirement e. ");
+            Console.WriteLine("==============================");
+
+            foreach (Athlete a in athletes)
+            {
+
+                if (a.EventCode == 'I')
+                {
+                    Console.WriteLine($"{a.Name} has entered an invalid event code");
+                }
+                else
+                {
+
+                   Console.WriteLine(a.ToString());
+                }
+
+            }
+
+
+
+
+
+        }
+    }
+}
